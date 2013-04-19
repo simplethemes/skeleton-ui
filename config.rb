517fa0a2175136174021142a231de5a22d013a02
Casey <casey@simplethemes.com>
2013-04-19 02:23:29 -0500

    updated callout styles

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-#output_style = :expanded
+output_style = :compressed
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
